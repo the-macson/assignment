ae717493d08661622383b61a267ed6b43adf92cf
the-macson <ak9991927952@gmail.com>
2024-06-22 12:19:45 +0530

    added remote url

--- lms-backend/src/config/db.config.js
+++ lms-backend/src/config/db.config.js
@@ -1,14 +1,24 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    port: process.env.DB_PORT,
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     port: process.env.DB_PORT,
+//   }
+// );
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 module.exports = sequelize;
